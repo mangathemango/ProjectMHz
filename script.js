acd1339e9648375f940c258e049f7e25a1ea87f1
mangathemango <163281872+mangathemango@users.noreply.github.com>
2024-09-27 19:19:30 +0800

    fixed logo

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ let r = new rive.Rive({
     src: "MHz.riv", // URL to the .riv file
     canvas: document.getElementById("canvas"),
     autoplay: true,
-    stateMachines: "State Machine 1", // Replace with your desired state machine name
-    onLoad: () => {
-      r.resizeDrawingSurfaceToCanvas(); // Resize the drawing surface to match the canvas
-    },
+    stateMachines: "State Machine 1" // Replace with your desired state machine name
   });
 
 document.getElementById("canvas").addEventListener("click", () => {
-    r.stateMachineInputs('State Machine 1').find(i => i.name === 'Loading Clicked').fire()
+    r.stateMachineInputs('State Machine 1').find(i => i.name === 'Loading Complete').fire()
 })
